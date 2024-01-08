5ef269bb496d36abad06593b816863997651ddd5
Wesley Lopes de Oliveira <x.wesleylopes@gmail.com>
2024-01-08 11:48:02 -0300

    refactor: change name

--- app/Views/admin/championships/index.php
+++ app/Views/admin/championships/index.php
@@ -19,7 +19,7 @@
           <div class="p-2 bg-gray-100 rounded-full">
             <i class="w-5 h-5 text-gray-600" data-feather="shield"></i>
           </div>
-          <h2 class="my-2 text-gray-600 font-semibold text-sm">Todos os times</h2>
+          <h2 class="my-2 text-gray-600 font-semibold text-sm">Times</h2>
           <p class="text-gray-400 text-center text-xs">Clique para visualizar os times deste campeonato</p>
         </div>
       </a>
